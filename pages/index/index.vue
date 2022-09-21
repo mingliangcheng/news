93fc73be8821c073e6cda37169b27c0c772893e7
chenmingliang <1825720338@qq.com>
2022-09-21 16:45:54 +0800

    项目初始化

---
+++ pages/index/index.vue
@@ -0,0 +1,105 @@
+<template>
+	<view class="container">
+		<uni-search-bar @confirm="search" :focus="true" v-model="keywords" @blur="blur" @focus="focus" @input="input"
+			cancelButton="none" @clear="clear" bgColor="#eee">
+		</uni-search-bar>
+		<custom-waterfalls-flow ref="waterfallsFlowRef" :value="list" :column="2" imageKey="image" :columnSpace="2">
+			<view class="item" v-for="(item,index) in list" :key="index" slot="slot{{index}}">
+				<image :src="item.image"></image>
+				<view class="title">{{item.title}}</view>
+				<view class="desc">{{item.desc}}</view>
+			</view>
+		</custom-waterfalls-flow>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				keywords: '',
+				list: [{
+						image: 'https://via.placeholder.com/200x500.png/ff0000',
+						title: '我是标题1',
+						desc: '描述描述描述描述描述描述描述描述1'
+					},
+					{
+						image: 'https://via.placeholder.com/200x200.png/2878ff',
+						title: '我是标题2',
+						desc: '描述描述描述描述描述描述描述描述2'
+					},
+					{
+						image: 'https://via.placeholder.com/200x100.png/FFB6C1',
+						title: '我是标题3',
+						desc: '描述描述描述描述描述描述描述描述3'
+					},
+					{
+						image: 'https://via.placeholder.com/200x300.png/9400D3',
+						title: '我是标题4',
+						desc: '描述描述描述描述描述描述描述描述4'
+					},
+					{
+						image: 'https://via.placeholder.com/100x240.png/B0E0E6',
+						title: '我是标题5',
+						desc: '描述描述描述描述描述描述描述描述5'
+					},
+					{
+						image: 'https://via.placeholder.com/140x280.png/7FFFAA',
+						title: '我是标题6',
+						desc: '描述描述描述描述描述描述描述描述6'
+					},
+					{
+						image: 'https://via.placeholder.com/40x60.png/EEE8AA',
+						title: '我是标题7',
+						desc: '描述描述描述描述描述描述描述描述7'
+					}
+				]
+			}
+		},
+		methods: {
+			clear() {
+
+			},
+			input() {
+
+			},
+			focus() {
+
+			},
+			blur() {
+
+			},
+			search() {
+
+			},
+			// 刷新数据
+			refresh() {
+				this.list = []; // 赋值
+				this.$refs.waterfallsFlowRef.refresh();
+			}
+		}
+	}
+</script>
+
+<style lang="scss">
+	.container {
+		padding: 20px;
+		font-size: 14px;
+		line-height: 24px;
+
+		.item {
+			padding: 10px 10px 20px;
+
+			.title {
+				line-height: 48px;
+				font-size: 28px;
+				color: #222;
+			}
+
+			.desc {
+				font-size: 24px;
+				color: #666;
+			}
+		}
+	}
+</style>
---
+++ pages/my/my.vue
@@ -0,0 +1,225 @@
+<template>
+	<view class="container">
+		<uni-card :is-shadow="false" :border="false" margin="0">
+			<view class="base-info">
+				<view class="desc">
+					<view class="nick-name">倾听他的事</view>
+					<view class="signature">每个人都有快乐的事，你呢？</view>
+				</view>
+				<view>
+					<image src="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg"></image>
+				</view>
+			</view>
+			<view class="other-info">
+				<view class="info-item">
+					<text class="number">97</text>
+					<text class="item-name">人气</text>
+				</view>
+				<view class="info-item">
+					<text class="number">977</text>
+					<text class="item-name">粉丝</text>
+				</view>
+				<view class="info-item">
+					<text class="number">197</text>
+					<text class="item-name">关注</text>
+				</view>
+			</view>
+		</uni-card>
+		<view class="middle-oprate">
+			<view class="operate-btn">
+				<uni-icons type="cloud-download" size="30"></uni-icons>
+				<text>历史下载</text>
+			</view>
+			<view class="operate-btn">
+				<uni-icons type="shop" size="30"></uni-icons>
+				<text>审核进度</text>
+			</view>
+			<view class="operate-btn">
+				<uni-icons type="headphones" size="30"></uni-icons>
+				<text>帮助反馈</text>
+			</view>
+			<view class="operate-btn">
+				<uni-icons type="gear" size="30"></uni-icons>
+				<text>设置中心</text>
+			</view>
+		</view>
+		<view class="works">
+			<view class="top-wrap">
+				<view class="my-works-btn">
+					<view class="my-btn-wrap">
+						<text>我的作品</text>
+					</view>
+					<view class="collect-btn">
+						<uni-icons type="heart-filled" size="15" color="#ccc"></uni-icons>
+					</view>
+				</view>
+				<view class="add-btn">
+					<uni-icons type="plusempty" size="15" color="#ccc"></uni-icons>
+				</view>
+			</view>
+		</view>
+		<view class="works-list-wrap">
+			<uni-card cover="https://fastly.jsdelivr.net/npm/@vant/assets/cat.jpeg" :is-shadow="false" :border="false" margin="0">
+				<!-- <image slot='cover' style="width: 100%;" :src="cover"></image> -->
+				<view class="uni-body">夜景</view>
+				<view class="uni-desc">家具</view>
+			</uni-card>
+		</view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+
+		}
+	}
+</script>
+
+<style lang="scss" scoped>
+	.container {
+		::v-deep .uni-card {
+			background-color: #f5f5f5;
+		}
+	}
+
+	.base-info {
+		display: flex;
+		justify-content: space-between;
+
+		.desc {
+			.nick-name {
+				font-size: 22px;
+				font-weight: 700;
+				color: #101010;
+				margin-bottom: 10px;
+			}
+
+			.signature {
+				color: #ccc;
+			}
+		}
+
+		image {
+			width: 100px;
+			height: 100px;
+			border-radius: 50%;
+		}
+	}
+
+	.other-info {
+		display: flex;
+
+		.info-item {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			margin-right: 30px;
+
+			.number {
+				font-size: 18px;
+				font-weight: 700;
+				color: #101010;
+				margin-bottom: 5px;
+			}
+
+			.item-name {
+				font-size: 14px;
+				color: #ccc;
+			}
+		}
+	}
+
+	.middle-oprate {
+		padding: 15px;
+		display: flex;
+
+		.operate-btn {
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+			width: 25%;
+
+			text {
+				font-size: 12px;
+			}
+		}
+	}
+
+	.works {
+		padding: 15px;
+
+		.top-wrap {
+			display: flex;
+			justify-content: space-between;
+			align-items: center;
+
+			.my-works-btn {
+				display: flex;
+				align-items: center;
+
+				.my-btn-wrap {
+					display: flex;
+					justify-content: center;
+					align-items: center;
+					background-color: #101010;
+					color: #fff;
+					width: 80px;
+					height: 30px;
+					border-radius: 15px;
+					font-size: 14px;
+					margin-right: 10px;
+				}
+
+				.collect-btn {
+					width: 30px;
+					height: 30px;
+					border-radius: 50%;
+					background-color: #ececec;
+					display: flex;
+					justify-content: center;
+					align-items: center;
+				}
+			}
+
+			.add-btn {
+				width: 30px;
+				height: 30px;
+				border-radius: 50%;
+				background-color: #ececec;
+				display: flex;
+				justify-content: center;
+				align-items: center;
+			}
+		}
+	}
+	.works-list-wrap {
+		padding: 15px;
+		display: flex;
+		::v-deep uni-card {
+			width: calc(50% - 5px);
+			&:nth-of-type(2n+1) {
+				margin-right: 10px;
+			}
+		}
+		::v-deep .uni-card {
+			background-color: #fff;
+		}
+		.uni-body {
+			font-size: 14px;
+			color: #101010;
+			font-weight: 700;
+		}
+		.uni-desc {
+			font-size: 14px;
+			color: #E2E2E2;
+		}
+	}
+</style>
